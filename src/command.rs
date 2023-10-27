f60b647acc03da76ee7858fcddbd9c841f408a84
Jibao Mansaray <jibaomansaray@gmail.com>
2023-10-27 13:44:13 +0000

    Expose the bus method for check if a handler exist

--- src/command.rs
+++ src/command.rs
@@ -24,10 +24,31 @@ pub trait DispatchableCommand: Send + Sync {
             .await;
     }
 
+    /// Register this handler if the command does not have an existing handler
+    async fn soft_command_handler<H: CommandHandler + Default + 'static>()
+    where
+        Self: Sized,
+    {
+        let bus = Busstop::instance();
+        if !bus.command_has_handler::<Self>().await {
+            bus.register_command::<Self>(H::default()).await;
+        }
+    }
+
     async fn register_command_handler<H: CommandHandler + 'static>(handler: H)
     where
         Self: Sized,
     {
         Busstop::instance().register_command::<Self>(handler).await;
+    }
+
+    async fn register_soft_command_handler<H: CommandHandler + 'static>(handler: H)
+    where
+        Self: Sized,
+    {
+        let bus = Busstop::instance();
+        if !bus.command_has_handler::<Self>().await {
+            bus.register_command::<Self>(handler).await;
+        }
     }
 }
--- src/query.rs
+++ src/query.rs
@@ -24,10 +24,30 @@ pub trait DispatchableQuery: Send + Sync {
             .await;
     }
 
+    /// Register this handler if the query does not have an existing handler
+    async fn soft_query_handler<H: QueryHandler + Default + 'static>()
+    where
+        Self: Sized,
+    {
+        let bus = Busstop::instance();
+        if !bus.query_has_handler::<Self>().await {
+            bus.register_query::<Self>(H::default()).await;
+        }
+    }
+
     async fn register_query_handler<H: QueryHandler + 'static>(handler: H)
     where
         Self: Sized,
     {
         Busstop::instance().register_query::<Self>(handler).await;
+    }
+    async fn register_soft_query_handler<H: QueryHandler + 'static>(handler: H)
+    where
+        Self: Sized,
+    {
+        let bus = Busstop::instance();
+        if !bus.query_has_handler::<Self>().await {
+            bus.register_query::<Self>(handler).await;
+        }
     }
 }
