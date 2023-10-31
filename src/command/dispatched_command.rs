991d4f3046b598eacdc7e193ce27bb877d2a12c8
Jibao Mansaray <jibaomansaray@gmail.com>
2023-10-31 11:25:18 +0000

    Require the return of the dispatched command
    Add a `handled` flag that indicates whether the command was handled.

--- src/command/dispatched_command.rs
+++ src/command/dispatched_command.rs
@@ -1,15 +1,29 @@
 use std::any::Any;
 
 #[derive(Debug)]
-pub struct DispatchedCommand(Box<dyn Any + Send + Sync>);
+pub struct DispatchedCommand {
+    inner: Box<dyn Any + Send + Sync>,
+    pub(crate) handled: bool,
+}
 
 impl DispatchedCommand {
     pub(crate) fn new(inner: Box<dyn Any + Send + Sync>) -> Self {
-        Self(inner)
+        Self {
+            inner,
+            handled: false,
+        }
     }
 
     /// Returns the inner (the real command) of the dispatched command
     pub fn the_command<T: 'static>(&self) -> Option<&T> {
-        self.0.downcast_ref()
+        self.inner.downcast_ref()
+    }
+
+    pub fn the_command_mut<T: 'static>(&mut self) -> Option<&mut T> {
+        self.inner.downcast_mut()
+    }
+
+    pub fn handled(&self) -> bool {
+        self.handled
     }
 }
