900f3dd2aa83fb5c82381c67d26fa00a11c717ab
Jibao Mansaray <jibaomansaray@gmail.com>
2024-02-21 07:33:34 +0000

    feat: Add the ability to take the command or query

--- src/command/dispatched_command.rs
+++ src/command/dispatched_command.rs
@@ -2,27 +2,46 @@ use std::any::Any;
 
 #[derive(Debug)]
 pub struct DispatchedCommand {
-    inner: Box<dyn Any + Send + Sync>,
+    inner: Option<Box<dyn Any + Send + Sync>>,
     pub(crate) handled: bool,
 }
 
 impl DispatchedCommand {
     pub(crate) fn new(inner: Box<dyn Any + Send + Sync>) -> Self {
         Self {
-            inner,
+            inner: Some(inner),
             handled: false,
         }
     }
 
-    /// Returns the inner (the real command) of the dispatched command
+    /// Returns a reference to (the real command)  the dispatched command
     pub fn the_command<T: 'static>(&self) -> Option<&T> {
-        self.inner.downcast_ref()
+        if let Some(inner) = &self.inner {
+            inner.downcast_ref()
+        } else {
+            None
+        }
     }
 
+    /// Returns a mutable reference to (the real command)  the dispatched command
     pub fn the_command_mut<T: 'static>(&mut self) -> Option<&mut T> {
-        self.inner.downcast_mut()
+        if let Some(inner) = &mut self.inner {
+            inner.downcast_mut()
+        } else {
+            None
+        }
     }
 
+    /// Returns (the real command)  the dispatched command
+    /// Subsequent to this method or `the_command_mut` and `the_command` will returned none
+    pub fn take_command<T: 'static>(&mut self) -> Option<Box<T>> {
+        if let Some(inner) = self.inner.take() {
+            return inner.downcast().ok();
+        }
+        None
+    }
+
+    /// Returns true if the command was handled
     pub fn handled(&self) -> bool {
         self.handled
     }
--- src/query/dispatched_query.rs
+++ src/query/dispatched_query.rs
@@ -2,35 +2,65 @@ use std::{any::Any, cell::OnceCell};
 
 #[derive(Debug)]
 pub struct DispatchedQuery {
-    query: Box<dyn Any + Send + Sync>,
-    value: OnceCell<Box<dyn Any + Send + Sync>>,
+    query: Option<Box<dyn Any + Send + Sync>>,
+    value: Option<OnceCell<Box<dyn Any + Send + Sync>>>,
     pub(crate) handled: bool,
 }
 
 impl DispatchedQuery {
     pub(crate) fn new(query: Box<dyn Any + Send + Sync>) -> Self {
         Self {
-            query,
-            value: OnceCell::new(),
+            query: Some(query),
+            value: Some(OnceCell::new()),
             handled: false,
         }
     }
 
-    /// Returns the inner (the real query) of the dispatched query
+    /// Returns a reference (the real query) of the dispatched query
     pub fn the_query<T: 'static>(&self) -> Option<&T> {
-        self.query.downcast_ref()
+        if let Some(query) = &self.query {
+            query.downcast_ref()
+        } else {
+            None
+        }
+    }
+
+    /// Returns the dispatched query
+    /// Subsequent call to this method or `the_query` will return none
+    pub fn take_query<T: 'static>(&mut self) -> Option<Box<T>> {
+        if let Some(query) = self.query.take() {
+            query.downcast().ok()
+        } else {
+            None
+        }
     }
 
     /// Sets the value that will be returned to the dispatcher
     pub fn set_value<V: Send + Sync + 'static>(&self, value: V) {
-        _ = self.value.set(Box::new(value));
+        if let Some(v) = &self.value {
+            _ = v.set(Box::new(value));
+        }
     }
 
     /// Returns the value set by the handler of the query
     pub fn value<T: 'static>(&self) -> Option<&T> {
-        self.value.get().unwrap().downcast_ref()
+        if let Some(v) = &self.value {
+            v.get().unwrap().downcast_ref()
+        } else {
+            None
+        }
     }
 
+    /// Returns the query's result value
+    pub fn take_value<T: 'static>(&mut self) -> Option<Box<T>> {
+        if let Some(v) = self.value.take() {
+            return v.into_inner().unwrap().downcast().ok();
+        }
+
+        None
+    }
+
+    /// Returns true if the query was handled
     pub fn handled(&self) -> bool {
         self.handled
     }
